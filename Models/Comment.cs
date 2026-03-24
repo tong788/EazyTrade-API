ccfb2dd5b054c3b0165177469bbfa519c04909ec
Supanut <t-supanut.won@axons.in.th>
2026-03-24 15:53:36 +0700

    fix(model):remove migration history and use scaffold instead

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,10 +1,25 @@
-namespace EazyTrade.Models
-{
-    public class Comment
-    {
-        public int Id { get; set; }
-        public string Detail { get; set; } = null!;
-        public Commodity Commodity { get; set; } = null!;
-    }
+﻿using System;
+using System.Collections.Generic;
 
+namespace EazyTrade.Models;
+
+public partial class Comment
+{
+    public int Id { get; set; }
+
+    public string? Detail { get; set; }
+
+    public int CommodityId { get; set; }
+
+    public int CreateBy { get; set; }
+
+    public DateTime CreateAt { get; set; }
+
+    public int UpdateBy { get; set; }
+
+    public DateTime UpdateAt { get; set; }
+
+    public DateTime? CancelDate { get; set; }
+
+    public virtual Commodity Commodity { get; set; } = null!;
 }
