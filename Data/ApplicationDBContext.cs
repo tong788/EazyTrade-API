8a86efe6108b0f5bf6fca81031681f280f2ce871
Supanut <t-supanut.won@axons.in.th>
2026-04-05 22:29:32 +0700

    fix: fix scaffold command to be no OnConfiguring

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -18,11 +18,7 @@ public partial class ApplicationDBContext : DbContext
 
     public virtual DbSet<Comment> Comments { get; set; }
 
-    public virtual DbSet<Commodity> Commodities { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=EazyTrade;Username=supernut_t;Password=29042547");
+asd    public virtual DbSet<Commodity> Commodities { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
