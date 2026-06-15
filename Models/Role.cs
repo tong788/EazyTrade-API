234572319112bbb944c872b85b2935ca735b2502
Supanut <t-supanut.won@axons.in.th>
2026-06-15 21:26:44 +0700

    feat: add role table for authorization

---
+++ Models/Role.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EazyTrade.Models;
+
+public partial class Role
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int CreateBy { get; set; }
+
+    public DateTime CreateAt { get; set; }
+
+    public int UpdateBy { get; set; }
+
+    public DateTime UpdateAt { get; set; }
+
+    public virtual ICollection<Account> Accounts { get; set; } = new List<Account>();
+}
