1d0dd02534c65876688e4d5d06b67134ccff23fa
Supanut <t-supanut.won@axons.in.th>
2026-06-18 23:14:36 +0700

    feat: add image file table for storing image metadata for cloud storage

---
+++ Models/ImageFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EazyTrade.Models;
+
+public partial class ImageFile
+{
+    public int Id { get; set; }
+
+    public string FileUrl { get; set; } = null!;
+
+    public string FileName { get; set; } = null!;
+
+    public long FileSize { get; set; }
+
+    public int CreateBy { get; set; }
+
+    public DateTime CreateAt { get; set; }
+
+    public int UpdateBy { get; set; }
+
+    public DateTime UpdateAt { get; set; }
+
+    public int ReferenceId { get; set; }
+
+    public string MimeType { get; set; } = null!;
+
+    public string ReferenceType { get; set; } = null!;
+}
